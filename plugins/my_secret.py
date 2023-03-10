404a3748fc7566c357171e1d0d940bfcf6a58e44
Jingchao Zhong <perry.zjc@gmail.com>
2023-03-10 02:46:55 -0800

    init

---
+++ plugins/my_secret.py
@@ -0,0 +1,17 @@
+"""
+This plugin searches for my secret
+"""
+import re
+
+from .base import RegexBasedDetector
+
+
+class MyDanger(RegexBasedDetector):
+    """Scans for my secret."""
+    secret_type = 'My custom secret'
+
+    denylist = [
+        # Discord Bot Token ([M|N]XXXXXXXXXXXXXXXXXXXXXXX.XXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXX)
+        # Reference: https://discord.com/developers/docs/reference#authentication
+        re.compile(r'danger\.pem')
+    ]
