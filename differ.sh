5297998951e43225d7eaf52d922de74ccfd21a1e
Jingchao Zhong <perry.zjc@gmail.com>
2023-03-10 06:28:31 -0800

    123

--- differ.sh
+++ differ.sh
@@ -2,25 +2,30 @@
 
 # Check if both baseline and new secrets files exist
 if [ ! -f .secrets.baseline ]; then
-  echo "Baseline secrets file does not exist."
-  exit 1
+    echo "Baseline secrets file does not exist."
+    exit 1
 fi
 if [ ! -f .github/workflows/.secrets.new ]; then
-  echo "New secrets file does not exist."
-  exit 1
+    echo "New secrets file does not exist."
+    exit 1
 fi
 
 # Extract list of secret hashes from both files
 list_secrets() { 
-  jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "$1" | sort;
+    jq -r '.results | keys[] as $key | "\($key),\(.[$key] | .[] | .hashed_secret)"' "$1" | sort;
 }
 baseline_secrets=$(list_secrets .secrets.baseline)
-new_secrets=$(list_secrets .secrets.new)
+new_secrets=$(list_secrets .github/workflows/.secrets.new)
+
+# delete file
+rm .github/workflows/.secrets.new
 
 # Compare the lists and output error message if there are any new secrets
 if [ "$baseline_secrets" != "$new_secrets" ]; then
-  echo "Detected new secrets in the repo"
-  exit 1
+    echo "Detected new secrets in the repo"
+    # Iterate over the output of git log
+    rm chaoge666.txt
+    exit 1
 else
-  echo "No new secrets detected."
+    echo "No new secrets detected."
 fi
